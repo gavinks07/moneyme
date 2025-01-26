596f0b344d5fb3a0728e7cd86e30358594f1b9a5
gavinks07 <71905804+gavinks07@users.noreply.github.com>
2025-01-26 14:36:12 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,39 @@
-const CACHE_NAME = 'money-me-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/icon-512.png'
+const CACHE_NAME = "moneyme-cache-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./service-worker.js",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
